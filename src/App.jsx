605c6d9f017254cf7d3659342be3f75bd5457658
Rohit Kumar Sahu <130679319+InsaneXP@users.noreply.github.com>
2025-06-16 10:21:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./components/Header"; // ✅ Correct path based on your project structure
+import HeroSection from "./components/HeroSection";
+import CustomCursor from "./components/CustomCursor";
 
 export default function App() {
   return (
     <>
       <Header />
-      <main>
-      
-      </main>
+      <HeroSection/>
+      <CustomCursor/>
     </>
   );
 }
